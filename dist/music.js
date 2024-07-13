61498c944fe47f7a3ebdee18dd7f4523076f26e1
ZYG <2339448077@qq.com>
2024-07-13 18:25:04 +0800

    Site updated: 2024-07-13 18:25:03

--- dist/music.js
+++ dist/music.js
@@ -10,7 +10,7 @@ const ap = new APlayer({
         {
             name: 'Without You (Explicit)',
             artist: 'Avicii/Sadro Cavazza',
-            url: 'https://gitee.com/zzzzzyg/sources/raw/master/WithotYou(Explicit).mp3',
+            url: 'https://down-load.lanrar.com/file/?VTNQbgg5AzICCwoyAzYCbgM8UGhfVwZtBHcBbwU2VSFQJAAOWWtSc1UrCxhTfwBxV2FVOV5iUToLeFN8XC4BbVUlUDQIcwMxAj0KMAMyAlwDMFAxXz0GNQQ6ATcFbVVnUGcAYFk2UjJVJQs7U3QAPFc6VWFeOFFgCz5TbVwwASZVJVB3CGgDZQJkCm4DYwIsA2lQYl8vBjUEMAEoBWFVMlAzADJZPVIwVWYLalMwADNXOVVgXjJRYQs0U2JcZgEwVTFQMghnA28CMgppAzUCYQNqUGxfZgZnBDABYgV3VTlQIABkWSJSc1VwCzhTdQBoV2lVbV4zUWQLO1NsXDgBNVVnUCEIIQM+AjkKOQMxAj4DaFBiXzIGNAQ7ATEFblVmUGQAZVkqUihVJQs7U2sAdlcwVWBeJ1EmC3xTIlw9ATFVYlA1CGUDbwJiCmkDYAI0A2FQc191Bm0EcwE6BWtVZlBjAHlZM1I/VS0Lb1MzADNXI1VhXjBRZQsiU3NcZAFvVSJQaQgKAzQCPwphA2c=',
             cover: '//y.qq.com/music/photo_new/T002R300x300M000003KjODP2FwTHe_2.jpg?max_age=2592000',
         }
     ]
